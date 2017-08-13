b96c3ba6fe28d05b2742bf65f8a5b1959bfbfaec
Simply Codin <SimplyCodin@users.noreply.github.com>
2017-08-13 13:40:10 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+(function () {
+  const remote = require('electron').remote;
+  const window = remote.getCurrentWindow();
+
+function init() {
+  document.getElementById("max-btn").addEventListener("click", function (e) {
+    if (!window.isMaximized()) {
+      window.maximize();
+    }
+    else {
+      window.minimize();
+    }
+  });
+  document.getElementById("close-btn").addEventListener("click", function (e) {
+    window.close();
+  });
+};
+
+document.onreadystatechange = function () {
+  if (document.readyState == "complete") {
+    init();
+  }
+};
+
+})();
